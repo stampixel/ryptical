ae1d336a71f5b695a69a21d7b48db18383a0b084
stampixel <stampixel.t@gmail.com>
2022-03-16 15:07:53 -0400

    commit on "master"

--- website/models.py
+++ website/models.py
@@ -10,10 +10,17 @@ class Link(db.Model):
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pfp = db.Column(db.String(500))
+    background = db.Column(db.String(500))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(24), unique=True, nullable=False)
     password = db.Column(db.String(150))
     links = db.relationship('Link')
+    profile = db.relationship('Profile')
 
 
--- website/views.py
+++ website/views.py
@@ -3,6 +3,7 @@ from flask import Blueprint, render_template, redirect, request, flash
 from flask_login import login_required, current_user # if the user is logged in, current_user will give us information on the user, such as name, password, links
 from .models import Link, User
 from . import db
+import validators
 
 views = Blueprint('views', __name__)
 
@@ -14,13 +15,29 @@ def home():
         url = request.form['url']
         display_url = request.form['display_url']
 
+        valid=validators.url(url)
+        print(valid)
+
         if len(url) < 1 or len(display_url) < 1:
             flash("Values can't be blank!", category='error')
+        if valid != True:
+            print("invalid")
+            url = f"https://{url}"
+            valid = validators.url(url)
+            if valid == True:
+                new_link = Link(url=url, display_url=display_url, user_id=current_user.id)
+                db.session.add(new_link)
+                db.session.commit()
+                flash('Link added!', category='success')
+            else:
+                flash('URL needs to be valid, example: https://tankated.ga', category='error')
         else:
             new_link = Link(url=url, display_url=display_url, user_id=current_user.id)
             db.session.add(new_link)
             db.session.commit()
             flash('Link added!', category='success')
+
+
     return render_template("editor.html", user=current_user) # we will be able to do stuff
 
 @views.route('/')
