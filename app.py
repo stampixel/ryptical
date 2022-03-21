10ac1bb53b1a582e5f91226b668de582c4f7a78f
stampixel <stampixel.t@gmail.com>
2022-03-21 09:56:46 -0400

    Updated host

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from website import create_app # __init__ makes a folder a python package, so we
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(port=3004)
+    app.run(port=3004, host="0.0.0.0")
