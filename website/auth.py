5becf18f4a69f34c7b3376fe4ca959a2caf1bf25
stampixel <stampixel.t@gmail.com>
2022-03-23 13:42:39 -0400

    Updated admin panel

--- website/auth.py
+++ website/auth.py
@@ -98,3 +98,4 @@ def admin_panel():
         password = request.form['password']
         if generate_password_hash(password, method='sha256') == "sha256$UcZ2R17piOwDygp3$3c334e6fcfd4a689ffab2d1a0de96cd26b169e5c99fb69cc86eed500dfd83edb":
             return render_template("admin_panel.html")
+    return render_template("admin.html") # make html for admin panel.
